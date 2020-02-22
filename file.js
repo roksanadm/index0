3406433b1bc037c3c4e1ced455b63d001a04122e
roksdm <roks.dm.hm@gmail.com>
2020-02-22 20:59:46 +0200

    style mob

---
+++ file.js
@@ -0,0 +1,9 @@
+@media screen and (min-device-width: 1600px) {
+    div {width: 1500px;}
+   }
+   @media screen and (device-width: 1280px) {
+    div {width: 1100px;}
+   }
+   @media screen and (device-width: 1024px) {
+    div {width: 980px;}
+   }
