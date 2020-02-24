e755e8b043fdbfff4690415769a7d8d33fee43e9
roksdm <roks.dm.hm@gmail.com>
2020-02-24 19:37:18 +0200

    grid site

--- 17.js
+++
@@ -1,6 +0,0 @@
-btn.addEventListener('click', function(e) {
-    var str = inpt.value;  // read value from input with id="str"
-    var res = str.split('').reverse().join(''); // reverse symbols in a string
-    result.innerHTML = res; // write result to span with is="result"
-    e.preventDefault(); // prevent form submission
-  });
